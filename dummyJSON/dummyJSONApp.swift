d2beee898ab4422c59695eec6b100ce937dd6a86
Lucas Faria <lucasfaria@MacBook-Pro-de-Lucas.local>
2024-04-11 10:07:08 +0100

    Initial commit - xcode project

---
+++ dummyJSON/dummyJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummyJSONApp.swift
+//  dummyJSON
+//
+//  Created by Lucas Faria on 10/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct dummyJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
